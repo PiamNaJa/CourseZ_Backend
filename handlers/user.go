a67bf8f46022f8895f0aaa03be8d1ce9a82371f1
palmKumpanat <kumpanat.joompabuth@gmail.com>
2022-12-30 22:32:52 +0700

    Login

--- handlers/user.go
+++ handlers/user.go
@@ -108,6 +108,47 @@ func RegisterTeacher(db *gorm.DB) fiber.Handler {
 	}
 }
 
+func LoginUser(db *gorm.DB) fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		var user *models.User
+
+		if err := c.BodyParser(&user); err != nil {
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"error": err.Error(),
+			})
+		}
+
+		password := user.Password
+
+		if err := db.Model(models.User{}).First(&user, user.Email).Error; err != nil {
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"error": err.Error(),
+			})
+		}
+
+		err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password))
+		if err != nil {
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"error": err.Error(),
+			})
+		}
+
+		token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+			"user_id": user.User_id,
+			"role":    user.Role,
+		})
+		tokenString, err := token.SignedString([]byte(os.Getenv("JWT_SECRET")))
+		if err != nil {
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"error": err.Error(),
+			})
+		}
+		return c.Status(fiber.StatusOK).JSON(fiber.Map{
+			"token": tokenString,
+		})
+	}
+}
+
 func UpdateStudent(db *gorm.DB) fiber.Handler {
 	return func(c *fiber.Ctx) error {
 		var user *models.User
@@ -116,7 +157,6 @@ func UpdateStudent(db *gorm.DB) fiber.Handler {
 				"error": err.Error(),
 			})
 		}
-
 
 		encryptPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), 10)
 		user.Password = string(encryptPassword)
