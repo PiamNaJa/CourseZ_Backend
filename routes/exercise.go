842e6c18f6cc423e13309c1bc8b3138ef95c0a97
PiamNaJa <peumza1150@gmail.com>
2023-01-19 22:36:47 +0700

    add middleware

--- routes/exercise.go
+++ routes/exercise.go
@@ -9,8 +9,8 @@ import (
 
 func ExerciseRoutes(app fiber.Router, db *gorm.DB) {
 	app.Post("/", m.IsLogin, m.IsTeacher, m.IsCourseOwner, m.IsVideoOwner, handlers.CreateExercise(db))
-	app.Get("/", m.IsLogin, m.IsCourseOwner, m.IsVideoOwner, handlers.GetAllExercise(db))
+	app.Get("/", handlers.GetAllExercise(db))
 	app.Get("/:exercise_id", handlers.GetExerciseById(db))
-	app.Delete("/:exercise_id", m.IsCourseOwner, m.IsVideoOwner, handlers.DeleteExerciseID(db))
+	app.Delete("/:exercise_id", m.IsLogin, m.IsTeacher, m.IsCourseOwner, m.IsVideoOwner, handlers.DeleteExerciseID(db))
 	app.Put("/:exercise_id", m.IsLogin, m.IsTeacher, m.IsCourseOwner, m.IsVideoOwner, handlers.UpdateExercise(db))
 }
