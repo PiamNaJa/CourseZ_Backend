6c98d32e906e425f48258f07bd6114e1e03316e1
PiamNaJa <peumza1150@gmail.com>
2023-01-19 22:36:55 +0700

    add middleware

--- middleware/course.go
+++ middleware/course.go
@@ -1,20 +1,45 @@
 package middleware
 
 import (
+	"os"
+	"time"
+
 	"github.com/PiamNaJa/CourseZ_Backend/configs"
 	"github.com/PiamNaJa/CourseZ_Backend/models"
 	"github.com/gofiber/fiber/v2"
+	"github.com/golang-jwt/jwt/v4"
 )
 
-func HaveCourse(c *fiber.Ctx) error {
-	var count int64
-	if err := configs.DB.Model(&models.Course{}).Where("course_id = ?", c.Params("course_id")).Count(&count).Error; err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-			"error": err.Error(),
+func IsCourseOwner(c *fiber.Ctx) error {
+	claims := jwt.MapClaims{}
+	_, err := jwt.ParseWithClaims(c.Get("authorization"), &claims, func(token *jwt.Token) (interface{}, error) {
+		return []byte(os.Getenv("JWT_Secret")), nil
+	})
+	if err != nil {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"error": "Unauthorized",
 		})
 	}
-	if count == 0 {
-		return c.SendStatus(fiber.StatusNotFound)
+	exp := claims["exp"].(float64)
+	if int64(exp) < time.Now().Unix() {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"error": "Token Expired",
+		})
+	}
+
+	var course *models.Course
+	if err := configs.DB.Model(&models.Course{}).Where("course_id = ?", c.Params("course_id")).First(&course).Error; err != nil {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"error": "Course not found",
+		})
+	}
+	courseOwner := course.TeacherID
+
+	owner := claims["teacher_id"].(float64)
+	if owner != float64(courseOwner) {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"error": "Unauthorized",
+		})
 	}
 	return c.Next()
 }
--- middleware/video.go
+++ middleware/video.go
@@ -1,21 +1,52 @@
 package middleware
 
 import (
+	"os"
+	"time"
+
 	"github.com/PiamNaJa/CourseZ_Backend/configs"
 	"github.com/PiamNaJa/CourseZ_Backend/models"
 	"github.com/gofiber/fiber/v2"
+	"github.com/golang-jwt/jwt/v4"
 )
 
-func HaveVideo(c *fiber.Ctx) error {
-	var count int64
-	if err := configs.DB.Model(&models.Video{}).Where("course_id = ? and video_id = ?", c.Params("course_id"), c.Params("video_id")).Count(&count).Error; err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-			"error": err.Error(),
+func IsVideoOwner(c *fiber.Ctx) error {
+	claims := jwt.MapClaims{}
+	_, err := jwt.ParseWithClaims(c.Get("authorization"), &claims, func(token *jwt.Token) (interface{}, error) {
+		return []byte(os.Getenv("JWT_Secret")), nil
+	})
+	if err != nil {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"error": "Unauthorized",
+		})
+	}
+	exp := claims["exp"].(float64)
+	if int64(exp) < time.Now().Unix() {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"error": "Token Expired",
 		})
 	}
 
-	if count == 0 {
-		return c.SendStatus(fiber.StatusNotFound)
+	var video *models.Video
+	if err := configs.DB.Model(&models.Video{}).Where("video_id = ?", c.Params("video_id")).First(&video).Error; err != nil {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"error": "Video not found",
+		})
+	}
+
+	var course *models.Course
+	if err := configs.DB.Model(&models.Course{}).Where("course_id = ?", video.CourseID).First(&course).Error; err != nil {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"error": "Course not found",
+		})
+	}
+	videoOwner := course.TeacherID
+
+	owner := claims["teacher_id"].(float64)
+	if owner != float64(videoOwner) {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"error": "Unauthorized",
+		})
 	}
 	return c.Next()
 }
