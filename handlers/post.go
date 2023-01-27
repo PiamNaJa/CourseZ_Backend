b74cf75d9c46941292197c225452101c7f7e15e4
palmKumpanat <kumpanat.joompabuth>
2023-01-27 23:43:08 +0700

    post cont

--- handlers/post.go
+++ handlers/post.go
@@ -32,3 +32,88 @@ func CreatePost(db *gorm.DB) fiber.Handler {
 		return c.Status(fiber.StatusCreated).JSON(&post)
 	}
 }
+
+func GetAllPost(db *gorm.DB) fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		var post *[]models.Post
+
+		if err := db.Model(&models.Post{}).Preload("Subject").Preload("User").Preload("Comments").Find(&post).Error; err != nil {
+			return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+				"error": "record not found",
+			})
+		}
+		return c.Status(fiber.StatusOK).JSON(&post)
+	}
+}
+
+func GetPostById(db *gorm.DB) fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		var post *models.Post
+		id := c.Params("post_id")
+
+		if err := db.Model(&models.Post{}).Preload("Subject").Preload("User").Preload("Comments").First(&post, id).Error; err != nil {
+			return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+				"error": "Post not found",
+			})
+		}
+		return c.Status(fiber.StatusOK).JSON(&post)
+	}
+}
+
+func GetPostBySubject(db *gorm.DB) fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		var post *[]models.Post
+
+		if err := db.Model(&models.Post{}).Where("subject_id = ?", c.Params("subject_id")).Find(&post).Error; err != nil {
+			return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+				"error": "No record",
+			})
+		}
+		return c.Status(fiber.StatusOK).JSON(&post)
+	}
+}
+
+func DeletePostByID(db *gorm.DB) fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		var post *models.Post
+		id := c.Params("post_id")
+
+		if err := db.Model(&models.Post{}).Delete(&post, id).Error; err != nil {
+			return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+				"error": "Post not found",
+			})
+		}
+		return c.Status(fiber.StatusOK).JSON("Deleted")
+	}
+}
+
+func UpdatePost(db *gorm.DB) fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		var post *models.Post
+		var updatePostData *models.Post
+		id := c.Params("post_id")
+
+		if err := db.Model(&models.Post{}).Preload("Subject").Preload("User").Preload("Comments").First(&post, id).Error; err != nil {
+			return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+				"error": "Post not found",
+			})
+		}
+
+		if err := c.BodyParser(&updatePostData); err != nil {
+			return c.Status(fiber.StatusNotAcceptable).JSON(fiber.Map{
+				"error": err.Error(),
+			})
+		}
+
+		post.Caption = updatePostData.Caption
+		post.Post_picture = updatePostData.Post_picture
+
+		if err := db.Save(&post).Error; err != nil {
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"error": err.Error(),
+			})
+		}
+
+		return c.Status(fiber.StatusOK).JSON("Update Success")
+	}
+}
--- routes/post.go
+++ routes/post.go
@@ -2,11 +2,16 @@ package routes
 
 import (
 	"github.com/PiamNaJa/CourseZ_Backend/handlers"
-
+	m "github.com/PiamNaJa/CourseZ_Backend/middleware"
 	"github.com/gofiber/fiber/v2"
 	"gorm.io/gorm"
 )
 
 func PostRoutes(app fiber.Router, db *gorm.DB) {
-	app.Post("/", handlers.CreatePost(db))
+	app.Post("/", m.IsLogin, handlers.CreatePost(db))
+	app.Get("/", handlers.GetAllPost(db))
+	app.Get("/:post_id", handlers.GetPostById(db))
+	app.Get("/getby/:subject_id", handlers.GetPostBySubject(db))
+	app.Put("/:post_id", m.IsLogin, m.IsCourseOwner, handlers.UpdatePost(db))
+	app.Delete("/:post_id", m.IsLogin, m.IsCourseOwner, handlers.DeletePostByID(db))
 }
