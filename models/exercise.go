a819eba584f170c749e04dd05b0eb7e4405d127b
palmKumpanat <98012331+palmKumpanat@users.noreply.github.com>
2023-02-02 14:21:54 +0700

    seed data more (#21)
    * seed Data
    
    * Edit reward_Item
    
    ---------
    
    Co-authored-by: palmKumpanat <kumpanat.joompabuth>

--- models/exercise.go
+++ models/exercise.go
@@ -2,7 +2,7 @@ package models
 
 type Exercise struct {
 	Exercise_id int32     `json:"exercise_id" gorm:"primaryKey;type:int"`
-	VideoID     int32     `json:"video_id" gorm:"index;type:int;not null" validate:"required,number"`     // VideoID is the id of the video
+	VideoID     int32     `json:"video_id" gorm:"index;type:int;not null" validate:"required,number"`       // VideoID is the id of the video
 	Question    string    `json:"question" gorm:"not null;type:varchar(1000)" validate:"required,max=1000"` // Question is the question of the exercise
 	Image       string    `json:"image" gorm:"type:varchar(5000)" validate:"max=5000"`                      // Image is a link to the image
 	Choices     *[]Choice `json:"choices" gorm:"not null;foreignKey:ExerciseID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
