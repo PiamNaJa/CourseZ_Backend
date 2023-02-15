54c08a8a70598687371623ee9d316bd91c82f464
Nachanon <98012263+PiamNaJa@users.noreply.github.com>
2023-02-15 16:52:37 +0700

    like course and  video (#24)

--- handlers/course.go
+++ handlers/course.go
@@ -105,3 +105,52 @@ func UpdateCourse(db *gorm.DB) fiber.Handler {
 		return c.Status(fiber.StatusOK).JSON("Update Success")
 	}
 }
+
+func LikeCourse(db *gorm.DB) fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		token := c.Get("authorization")
+		if token == "" {
+			return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+				"error": "Unauthorized",
+			})
+		}
+		claims, err := constants.GetClaims(token)
+		if err != nil {
+			return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+				"error": "Unauthorized",
+			})
+		}
+		var user models.User
+		if err := db.Model(&models.User{}).Preload("LikeCourses").First(&user, claims["user_id"]).Error; err != nil {
+			return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+				"error": "User not found",
+			})
+		}
+		var course models.Course
+		id := c.Params("course_id")
+
+		if err := db.First(&course, &id).Error; err != nil {
+			return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+				"error": "Course not found",
+			})
+		}
+
+		course.Like += 1
+		user.LikeCourses = append(user.LikeCourses, &course)
+		tx := db.Begin()
+		if err := tx.Save(&course).Error; err != nil {
+			tx.Rollback()
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"error": err.Error(),
+			})
+		}
+		if err := tx.Save(&user).Error; err != nil {
+			tx.Rollback()
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"error": err.Error(),
+			})
+		}
+		tx.Commit()
+		return c.Status(fiber.StatusOK).JSON("Like Success")
+	}
+}
