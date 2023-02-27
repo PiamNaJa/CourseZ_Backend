78d0f74f16630dcc61361676e64ca795af63bf06
Nachanon <98012263+PiamNaJa@users.noreply.github.com>
2023-02-27 22:39:35 +0700

    post to patch

--- routes/exercise.go
+++ routes/exercise.go
@@ -11,7 +11,7 @@ func ExerciseRoutes(app fiber.Router, db *gorm.DB) {
 	app.Post("/", m.IsLogin, m.IsTeacher, m.IsCourseOwner, m.IsVideoOwner, handlers.CreateExercise(db))
 	app.Get("/", handlers.GetAllExercise(db))
 	app.Get("/isdone", m.IsLogin, handlers.IsDoneExercise(db))
-	app.Post("/done", m.IsLogin, handlers.AddPointAndDoneExercise(db))
+	app.Patch("/done", m.IsLogin, handlers.AddPointAndDoneExercise(db))
 	app.Get("/:exercise_id", handlers.GetExerciseById(db))
 	app.Delete("/:exercise_id", m.IsLogin, m.IsTeacher, m.IsCourseOwner, m.IsVideoOwner, handlers.DeleteExerciseID(db))
 	app.Put("/:exercise_id", m.IsLogin, m.IsTeacher, m.IsCourseOwner, m.IsVideoOwner, handlers.UpdateExercise(db))
