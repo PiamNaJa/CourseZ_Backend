f97f0acc1c3bd29bdc776e643857664d4603cff6
Nachanon <98012263+PiamNaJa@users.noreply.github.com>
2023-03-09 00:01:47 +0700

    add coursehistory

--- handlers/history.go
+++ handlers/history.go
@@ -14,7 +14,7 @@ func CheckUserVideoHistory(db *gorm.DB) fiber.Handler {
 		if err != nil {
 			return utils.Unauthorized(err.Error())
 		}
-		var history models.History
+		var history models.VideoHistory
 		if err := db.Where("user_id = ? AND video_id = ?", claims["user_id"], c.Params("video_id")).First(&history).Error; err != nil {
 			return utils.HandleFindError(err)
 		}
@@ -30,14 +30,14 @@ func AddVideoHistory(db *gorm.DB) fiber.Handler {
 			return utils.Unauthorized(err.Error())
 		}
 
-		var history models.History
+		var history models.VideoHistory
 		if err := c.BodyParser(&history); err != nil {
 			return utils.BadRequest(err.Error())
 		}
 		videoDuration := history.Duration
 
 		history.UserID = int32(claims["user_id"].(float64))
-		result := db.FirstOrCreate(&history, models.History{UserID: history.UserID, VideoID: history.VideoID})
+		result := db.FirstOrCreate(&history, models.VideoHistory{UserID: history.UserID, VideoID: history.VideoID})
 		if result.Error != nil {
 			return utils.BadRequest(result.Error.Error())
 		}
@@ -46,6 +46,17 @@ func AddVideoHistory(db *gorm.DB) fiber.Handler {
 			if err := db.Save(&history).Error; err != nil {
 				return utils.BadRequest(err.Error())
 			}
+		}
+		var CourseHistory models.CourseHistory
+		var video models.Video
+		if err:= db.Where("video_id = ?", history.VideoID).First(&video).Error; err != nil {
+			return utils.BadRequest(err.Error())
+		}
+		CourseHistory.UserID = history.UserID
+		CourseHistory.CourseID = video.CourseID
+		result = db.FirstOrCreate(&CourseHistory, models.CourseHistory{UserID: CourseHistory.UserID, CourseID: CourseHistory.CourseID})
+		if result.Error != nil {
+			return utils.BadRequest(result.Error.Error())
 		}
 		return c.Status(fiber.StatusCreated).JSON(history)
 	}
