0717d1b730a88cf29096524de0e77bfde3ee3928
Nachanon <98012263+PiamNaJa@users.noreply.github.com>
2023-03-16 20:15:24 +0700

    Get Payment Transaction By Teacher

--- models/payment.go
+++ models/payment.go
@@ -6,7 +6,9 @@ type Payment struct {
 	Payee      *User        `json:"payee" gorm:"foreignKey:UserID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
 	TeacherID  int32        `json:"recipient(teacher_id)" gorm:"index;type:int" validate:"required,number"`
 	Recipient  *UserTeacher `json:"recipient" gorm:"not null;foreignKey:TeacherID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
-	Money      int32        `json:"money" gorm:"type:int;default:0 "`
+	Money      int32        `json:"money" gorm:"type:int;default:0"`
 	Text       string       `json:"text" gorm:"not null;type:varchar(100)" validate:"required,max=100"`
+	VideoID    int32        `json:"video_id" gorm:"index;type:int" validate:"required,number"`
+	Video      *Video       `json:"video" gorm:"foreignKey:VideoID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
 	CreatedAt  int64        `json:"created_at" gorm:"autoCreateTime"`
 }
