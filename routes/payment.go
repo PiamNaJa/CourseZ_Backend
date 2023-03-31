e3acf265de6220835e5e6a547eff0c47b28a9ca4
Nachanon <98012263+PiamNaJa@users.noreply.github.com>
2023-03-31 18:20:23 +0700

    GetPaidVideosObject (#54)

--- routes/payment.go
+++ routes/payment.go
@@ -10,5 +10,6 @@ import (
 func PaymentRoutes(app fiber.Router, db *gorm.DB) {
 	app.Post("/videos", m.IsLogin, handlers.VideosPayment(db))
 	app.Get("/paid/videos", m.IsLogin, handlers.GetPaidVideos(db))
+	app.Get("/paid/videos/object", m.IsLogin, handlers.GetPaidVideosObject(db))
 	app.Get("/teacher/:teacher_id", m.IsLogin, m.IsTeacher , handlers.GetPaymentTransaction(db))
 }
