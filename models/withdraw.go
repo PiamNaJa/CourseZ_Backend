eb3faf4495b1ad21436635d0c44422749c25a66e
Nachanon <98012263+PiamNaJa@users.noreply.github.com>
2023-04-05 22:57:21 +0700

    Withdraw2 (#57)
    * withdraw fix
    
    * change return status to 201

--- models/withdraw.go
+++ models/withdraw.go
@@ -1,10 +1,10 @@
 package models
 
 type Withdraw struct {
-	Withdraw_id int32        `json:"withdraw_id" gorm:"primaryKey;type:int"`
-	TeacherID   int32        `json:"recipient(teacher_id)" gorm:"index;type:int" validate:"required,number"`
-	Recipient   *UserTeacher `json:"recipient" gorm:"not null;foreignKey:TeacherID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
-	Money       int32        `json:"money" gorm:"type:int;default:0 "`
-	Text        string       `json:"text" gorm:"not null;type:varchar(100)" validate:"required,max=100"`
-	CreatedAt   int64        `json:"created_at" gorm:"autoCreateTime"`
+	Withdraw_id int32  `json:"withdraw_id" gorm:"primaryKey;type:int"`
+	TeacherID   int32  `json:"recipient(teacher_id)" gorm:"index;type:int" validate:"required,number"`
+	Money       int32  `json:"money" gorm:"type:int;default:0 "`
+	BankName    string `json:"bankname" gorm:"not null;type:varchar(12)" validate:"required"`
+	BankNumber  string `json:"banknumber" gorm:"not null;type:varchar(20)" validate:"required,max=10,min=10"`
+	CreatedAt   int64  `json:"created_at" gorm:"autoCreateTime"`
 }
