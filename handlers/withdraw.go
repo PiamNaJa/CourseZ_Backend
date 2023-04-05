e7f3f551eeaf88e592c763c1501c0979160a395d
Nachanon <98012263+PiamNaJa@users.noreply.github.com>
2023-04-05 16:49:58 +0700

    withdraw fix

--- handlers/withdraw.go
+++ handlers/withdraw.go
@@ -14,13 +14,11 @@ func WithdrawMoney(db *gorm.DB) fiber.Handler {
 		if err != nil {
 			return utils.Unauthorized(err.Error())
 		}
-
-		var request map[string]interface{}
-		if err := c.BodyParser(&request); err != nil {
+		var withdraw models.Withdraw
+		if err := c.BodyParser(&withdraw); err != nil {
 			return utils.BadRequest(err.Error())
 		}
-
-		var money int32 = int32(request["money"].(float64))
+		money := withdraw.Money
 		tx := db.Begin()
 		var teacher models.UserTeacher
 		if err := tx.Where("teacher_id = ?", claims["teacher_id"]).First(&teacher).Error; err != nil {
@@ -37,12 +35,7 @@ func WithdrawMoney(db *gorm.DB) fiber.Handler {
 			tx.Rollback()
 			return utils.Unexpected(err.Error())
 		}
-
-		var withdraw models.Withdraw
-		withdraw.Recipient = &teacher
-		withdraw.Money = money
-		withdraw.Text = "Withdraw money"
-
+		withdraw.TeacherID = teacher.Teacher_id
 		if err := tx.Create(&withdraw).Error; err != nil {
 			tx.Rollback()
 			return utils.Unexpected(err.Error())
